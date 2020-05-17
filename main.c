c6e9eb076c16e82bd9b2c64fef50817fc9fb53ab
Alparslan ÇAY <50846539+alparslancay@users.noreply.github.com>
2020-05-17 21:50:05 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+typedef struct{
+    int position;
+    int count;
+    sem_t *forks;
+    sem_t *lock;
+} params_t;
+
+void initializeSemaphores(sem_t *lock, sem_t *forks, int num_forks);
+void runAllThreads(pthread_t *threads, sem_t *forks, sem_t *lock, int num_philosophers);
+
+void *philosopher(void *params);
+void think(int position);
+void eat(int position);
+
+int main(int argc, char *args[])
+{
+    int num_philosophers = 5;
+    sem_t lock;
+    sem_t forks[num_philosophers];
+    pthread_t philosophers[num_philosophers];
+
+    initializeSemaphores(&lock, forks, num_philosophers);
+    runAllThreads(philosophers,forks,&lock,num_philosophers);
+    pthread_exit();
+    return 0;
+}
+
+void initializeSemaphores(sem_t *lock, sem_t *forks, int num_forks){
+
+int i;
+    for(i=0;i<num_forks;i++)
+        sem_init(&forks[i],0,1);//TODO
+    sem_init(lock,0,num_forks -1);//TODO
+}
+
+void runAllThreads(pthread_t *threads, sem_t *forks, sem_t *lock, int num_philosophers){
+int i;
+    for(i=0; i<num_philosophers; i++){
+    params_t *arg = malloc(sizeof(params_t));
+    arg->position = i;
+    arg->count = num_philosophers;
+    arg->lock = lock;
+    arg->forks = forks;
+
+    pthread_create(&threads[i],NULL,philosopher, (void *)arg);
+    }
+
+}
+
+void *philosopher(void *params){
+int i;
+params_t self = *(params_t params);
+    for(i=0; i<3; i++){
+        think(self.position);
+        sem_wait(self.lock);
+        sem_wait(&self.forks[self.position]);
+        sem_wait(&self.forks[(self.position+1)%self.count)];
+        eat(self.position);
+        sem_post(&self.forks[self.position]);
+        sem_post(&self.forks[(self.position+1)%self.count]);
+        sem_post(self.lock);
+    }
+    think(self.position);
+    pthread_exit(NULL);
+}
+
+void think(int position){
+printf("Philosopher %d thinking...\n",position);
+}
+
+void eat(int position){
+printf("Philosopher %d eating...\n", position);
+}
