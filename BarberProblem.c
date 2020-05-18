3ed3f591b8f5ed2cb1448292b5547e29edaf331c
DoanKasas <46919728+DoanKasas@users.noreply.github.com>
2020-05-18 21:18:27 +0300

    Added comments and deleted some unused rows

--- BarberProblem.c
+++ BarberProblem.c
@@ -4,46 +4,39 @@
 #include <semaphore.h>
 #include <time.h>
 
-sem_t waitingArea; // Bekleme Alani Semaforu
-sem_t shavingArea; // Tras Semaforu
-sem_t barberSleeping; // Berber Uyumsa Semaforu
-sem_t barberWaiting;
+sem_t waitingArea; // Bekleme alanı Semaforu
+sem_t shavingArea; // Traş koltuğu Semaforu
+sem_t barberSleeping; // Berber uyuma Semaforu
+sem_t barberWaiting; //Müşteri bekleme semaforu
 
-pthread_mutex_t mutex;
-
-int currentNumberCustomers = 0;
+int currentNumberCustomers = 0; //Anlık müşteri 
 
 void *Barber();
 void *Customer();
 
-int currentCustomerNumber = -1;
+int currentCustomerNumber = -1; //Müşteri sıra numarası
 
 int main()
 {
-    int isBarberSleeping = 1;
-    int i; //Ortak for döngüleri için.
+    int i; 
     srand(time(NULL));
-    int thisDayNumberCustomers = 5 + rand()%11; // Gün boyunca 5 ile 15 arasý müþteri gelmesi isteniyor.
+    int thisDayNumberCustomers = 5 + rand()%11; // Gün boyunca 5 ile 15 arası müşteri gelmesi isteniyor.
     printf("Müşteri sayısı: %d\n",thisDayNumberCustomers);
 
-    pthread_t customerThreads[thisDayNumberCustomers], barberThread; // Berber ve müþteri Thread'leri tanýmlandý.
-    //Semaforlar tanýmlanýyor.
-    sem_init(&waitingArea,1,5); // Bekleme alanýnýn müþteri limiti 5 ayarlandý.
-    sem_init(&shavingArea,1,1); // Traþ için müþteri limiti 1 ayarlandý.
-    sem_init(&barberSleeping,1,0); // Berber uyuma semaforu konuldu.
-    sem_init(&barberWaiting,1,0); // Berberin müşteriyi bekleme semaforu konuldu.
+    pthread_t customerThreads[thisDayNumberCustomers], barberThread; // Berber ve müşteri Thread'leri tanımlandı.
+    //Semaforlar tanımlanıyor.
+    sem_init(&waitingArea,1,5); // Bekleme alanının müşteri limiti 5 ayarlandı.
+    sem_init(&shavingArea,1,1); // Traş alanının müşteri limiti 1 ayarlandı.
+    sem_init(&barberSleeping,1,0); // Berber uyuma semaforu ayarlandı.
+    sem_init(&barberWaiting,1,0); // Berberin müşteriyi bekleme semaforu ayarlandı.
 
-
-    //Thread'ler oluþturuluyor
-
-    pthread_mutex_init(&mutex,NULL);
-
+    //Thread'ler oluşturuluyor
     pthread_create(&barberThread,NULL,Barber,NULL);
 
     for(i=0; i< thisDayNumberCustomers;i++)
         pthread_create(&customerThreads[i],NULL,Customer,(void *)i+1);
 
-    //Join iþlemi çökme olmasýn diye create'lerden ayrý yazýlýyor.
+    //Join işlemi çökme olmasın diye create'lerden ayrı yazılıyor.
     pthread_join(barberThread,NULL);
 
     for(i=0; i< thisDayNumberCustomers;i++)
@@ -54,42 +47,41 @@ int main()
 }
 
 void *Customer(void * customerNumber){
-sleep(5 + rand()%25);
+sleep(5 + rand()%25);// Müşterilerin dükkana gelme zaman aralığı
 
 printf("\n %d.numarali Müşteri berber dükkanının önüne geldi.\n",(int)customerNumber);
 
+sem_wait(&waitingArea);// Müşteri bekleme alanı için sıraya giriyor.
 
-sem_wait(&waitingArea);
-
-currentNumberCustomers++;
+currentNumberCustomers++;// Girdikten sonra  mevcut müşteri sayısı 1 arttırılıyor.
 
 printf("%d numaralı Müşteri bekleme alanına geçti.\n",(int)customerNumber);
 
-if(currentNumberCustomers == 1)
+if(currentNumberCustomers == 1) // Eğer ilk müşteri ise
 {
     printf("%d numaralı müşteri berberi uyandirdi.\n",(int)customerNumber);
-    sem_post(&barberSleeping);
+    sem_post(&barberSleeping);//Berber uyandırılıyor
 }
-sem_wait(&shavingArea);
+sem_wait(&shavingArea);// Müşteri traş alanı için sıraya giriyor.
 
-sem_post(&waitingArea);
+sem_post(&waitingArea);// Müşteri bekleme alanından çıkıyor.
 
 currentCustomerNumber = (int)customerNumber;
 
 printf("%d numaralı Müşteri traş olma alanına geçti.\n",(int)customerNumber);
 
-sem_post(&barberWaiting);
+sem_post(&barberWaiting);// Müşteri traş için hazır.
 }
 
 void *Barber(){
-while(1){
+while(1){ 
 
-if(currentNumberCustomers <= 0){
+if(currentNumberCustomers <= 0){//Eğer müşteri kalmadıysa
     printf("Barber Sleeping\n");
-    sem_wait(&barberSleeping);
+    sem_wait(&barberSleeping);//Berber uyutulur.
 }
 
-sem_wait(&barberWaiting);
+sem_wait(&barberWaiting);// Berber traş için hazır.
 
 printf("\nAnlık müşteri sayısı: %d\n",currentNumberCustomers);
 
@@ -97,11 +89,11 @@ printf("%d.Müşteri Traş oluyor.\n",currentCustomerNumber);
 sleep(1);
 printf("%d.Müşteri traş oldu.\n",currentCustomerNumber);
 
-currentNumberCustomers--;
+currentNumberCustomers--;// Anlık müşteri sayısı 1 azaltılıyor.
 
 printf("%d.Müşteri dükkandan çıktı.\n",currentCustomerNumber);
 
-sem_post(&shavingArea);
+sem_post(&shavingArea);// Müşteri traş koltuğundan ayrılıyor.
 
 
 }
